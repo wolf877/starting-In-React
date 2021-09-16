efd68a2d4d08bbbdd17bec3e8de32919ee9fa930
William Santos <williamcnh1@gmail.com>
2021-09-16 13:15:51 -0300

    add firebase

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-
+import './service/firebase.ts';
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/service/firebase.ts
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from  'firebase/auth' 
+import {getDatabase} from 'firebase/database'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD78ZHyLtBBNnrkLQQZX3tgj3-u4Lpaw0E",
+  authDomain: "projeto001-f1718.firebaseapp.com",
+  databaseURL: "https://projeto001-f1718-default-rtdb.firebaseio.com",
+  projectId: "projeto001-f1718",
+  storageBucket: "projeto001-f1718.appspot.com",
+  messagingSenderId: "104261187857",
+  appId: "1:104261187857:web:ff3ba23c984af354e734b0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+export const auth = getAuth(app);
+export const database = getDatabase(app);
